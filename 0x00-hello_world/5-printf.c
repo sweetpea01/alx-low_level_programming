3b0d8bf69349a4f447bde8ee11f2c24f173100bc
sweetpea01 <kiruguderick.45@gmail.com>
2023-06-20 07:36:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+* main - using the printf function to print words
+* Return: 0 (success)
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
