2644aa492b9177b38749540f52807f21913cea09
sweetpea01 <kiruguderick.45@gmail.com>
2023-06-21 03:29:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
 * main - this is a program that prints the following output
 * and that piece of art is useful" - Dora Korpar, 2015-10-19
@@ -7,6 +8,6 @@
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" -Dora kopar, 2015-10-19"\n, 59);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n, stdout);
 return (1);
 }
