57c10be5255134ae85a1c545982aa7b6c460f92f
sweetpea01 <kiruguderick.45@gmail.com>
2023-06-21 00:18:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 /**
 * main - A program that prints the sizeof a data type
-* Return 0 (success)
+* on the computer it is compiled on
+*Return:(0)sucesss
 */
 int main(void)
 {
@@ -15,5 +16,5 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+return ("0");
 }
