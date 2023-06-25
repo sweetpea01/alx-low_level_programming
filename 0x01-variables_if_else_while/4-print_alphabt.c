df9272c90cb5dbbf4115caa938f62f9ab3d693e2
sweetpea01 <kiruguderick.45@gmail.com>
2023-06-25 05:09:55 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,14 +5,14 @@
 */
 int main(void)
 {
-  char i;
-  for (i ='a'; i <= 'z'; i++)
-    {
-      if (i != 'e' && i != 'q')
-	{
-	  putchar(i);
-	}
-    }
-  putchar('\n');
-  return (0);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'e' && i != 'q')
+{
+putchar(i);
+}
+}
+putchar('\n');
+return (0);
 }
