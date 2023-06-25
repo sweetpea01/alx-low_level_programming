d1091e41554e84e5ddfd3a0c470d323acbf983d5
sweetpea01 <kiruguderick.45@gmail.com>
2023-06-24 21:05:08 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - This is the entry point for the program
+* Return: 0 This always means (Success)
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while
+(c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
