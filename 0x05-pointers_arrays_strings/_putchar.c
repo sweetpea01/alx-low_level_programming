0e2be6991c90a08ddf11ee47d45868e9b2767a24
sweetpea01 <kiruguderick.45@gmail.com>
2023-07-03 18:53:03 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+*  _putchar - This is useful for writing a character to the standard output  
+* Return: Always return the number of characters written on (Success)   
+*/
+int _putchar(char d)
+{
+return write (1, &d, 1);
+}
